b824442a94d65ca086d9b03f9cc114da9473dd59
mattias421 <mcross2@sheffield.ac.uk>
2025-04-21 10:37:55 +0000

    orion hpopt works

--- slurm/hpopt_prelim.mima.sh
+++ slurm/hpopt_prelim.mima.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WANDB_TAGS=("hpopt" "prelim_loss_hpopt")
-orion hunt -n prelim_loss_hpoptv2 -c hparams/orion.yaml python train.py hparams/train.yaml --precision=fp16 --eval_precision=fp16 --data_folder=$DATA --hpopt hparams/hpopt.yaml --hpopt_mode orion \
+orion hunt -n prelim_loss_hpoptv3 -c hparams/orion.yaml python train.py hparams/train.yaml --precision=fp16 --eval_precision=fp16 --data_folder=$DATA --hpopt hparams/hpopt.yaml --hpopt_mode orion \
   --cfm_sigma~"choices([0.01, 0.05, 0.1, 0.2, 0.3])" \
   --batch_size~"choices([16, 32, 64])" \
   --learning_rate~"choices([0.001, 0.005, 0.01, 0.0005])" \
