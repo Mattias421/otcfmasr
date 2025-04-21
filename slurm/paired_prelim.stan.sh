47f0d7bffc59df7a447c9ea2d9df60bdbe2818dd
mattias421 <mcross2@sheffield.ac.uk>
2025-04-21 12:47:38 +0000

    quickfix

--- slurm/paired_prelim.stan.sh
+++ slurm/paired_prelim.stan.sh
@@ -16,9 +16,9 @@ source .venv/bin/activate
 
 name="paired_prelim"
 
-WANDB_TAGS=("hpopt" $paired_prelim)
-orion hunt -n $paired_prelim -c hparams/orion.yaml --n-workers 4 python train.py hparams/train.yaml --precision=fp16 --eval_precision=fp16 --data_folder=$DATA --hpopt hparams/hpopt.yaml --hpopt_mode orion \
-  --xp_name=$paired_prelim \
+WANDB_TAGS=("hpopt" $name)
+orion hunt -n $name -c hparams/orion.yaml --n-workers 4 python train.py hparams/train.yaml --precision=fp16 --eval_precision=fp16 --data_folder=$DATA --hpopt hparams/hpopt.yaml --hpopt_mode orion \
+  --xp_name=$name \
   --cfm_sigma~"choices([0.01, 0.05, 0.1, 0.2, 0.3])" \
   --batch_size~"choices([16, 32, 64])" \
   --learning_rate~"choices([0.001, 0.005, 0.01, 0.0005])" \
